ce27469830e11e89d84200d97920ee90ae1193ae
reasat-rafio <rafioar96@gmail.com>
2021-10-17 23:44:36 +0600

    :sparkles: container added

---
+++ apps/frontend/src/components/ui/container.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx';
+interface Props {
+  className?: string;
+  children?: any;
+  el?: HTMLElement;
+  clean?: boolean;
+}
+
+const Container: React.FC<Props> = ({
+  children,
+  className,
+  el = 'div',
+  clean,
+}) => {
+  const rootClassName = clsx(
+    className,
+    !clean && 'mx-auto max-w-[1920px] px-4 md:px-8 2xl:px-16'
+  );
+
+  const Component: React.ComponentType<React.HTMLAttributes<HTMLDivElement>> =
+    el as any;
+
+  return <Component className={rootClassName}>{children}</Component>;
+};
+
+export default Container;
